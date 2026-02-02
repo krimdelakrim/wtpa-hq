8aabaf73d07ef8575f661aef2c3cda8a11d5a93b
krimdelakrim <jamestribaltrust@gmail.com>
2026-02-01 21:34:45 -0500

    Fix agreement page export (Next module)

---
+++ app/(auth)/portal/agreement/page.tsx
@@ -0,0 +1,9 @@
+export default function AgreementPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Agreement</h1>
+      <p>Agreement content goes here.</p>
+    </main>
+  );
+}
+
