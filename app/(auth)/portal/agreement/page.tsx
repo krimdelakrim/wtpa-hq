de00283b32e264b84cb59e3b51f65f360b44ecb6
krimdelakrim <jamestribaltrust@gmail.com>
2026-02-01 21:39:32 -0500

    Recreate agreement page cleanly

--- app/(auth)/portal/agreement/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AgreementPage() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Agreement</h1>
-      <p>Agreement content goes here.</p>
-    </main>
-  );
-}
-
